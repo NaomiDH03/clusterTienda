80c3fd6f65af4f415beb580abe605a8c9e079d65
Naomi <naomidh2003@gmail.com>
2024-11-30 12:32:44 -0600

    Instalacion paquetes

---
+++ clusterTienda.Shared/Entities/Icecream.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clusterTienda.Shared.Entities
+{
+    public class Icecream
+    {
+        public int Id { get; set; } //El ID en automatico es autonumerico y es un key
+        [Required] //Esto significa que será un campo requerido, o sea, obligatorio
+        [MaxLength(100, ErrorMessage = "El campo{0} debe tener máximo {1} caracteres ")]
+        //Nota: El required y MaxLenght solo afecta a la propiedad siguiente, o sea, Flavour
+        [Display(Name = "Helado")]
+        public string Flavour { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public float Price { get; set; }
+
+    }
+}
---
+++ clusterTienda.Shared/Entities/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clusterTienda.Shared.Entities
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        [Required] //Esto significa que será un campo requerido, o sea, obligatorio
+        [MaxLength(100, ErrorMessage = "El campo{0} debe tener máximo {1} caracteres ")]
+        //Nota: El required y MaxLenght solo afecta a la propiedad siguiente, o sea, Flavour
+        [Display(Name = "Tienda")]
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Location { get; set; } = null!;
+
+    }
+}
